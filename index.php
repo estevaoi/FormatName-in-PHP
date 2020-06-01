b5fed8b62c9918309ac0370532c6aa4e92a5e359
estevaoi <estevaonorberto@gmail.com>
2020-06-01 15:01:10 -0300

    Created function

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
-# FormatName-in-PHP
-## Formatação de nomes me PHP para padronização no formato
+<?php
 
-A finalidade desta função é tratar um nome qualquer (nome de pessoa) e padronizá-lo, por exemplo:
-
-1. JOSÉ NORBERTO E SILVA: José Norberto e Silva
-2. ÂNTONIO sOBRInHO Dos Santos: Ântonio Sobrinho dos Santos
-3. Jéssica Cunha BraGA Da Costa: Jéssica Cunha Braga da Costa
-
-É isso aí, espero que ajude.
-
-
-```PHP
 function formatName($name)
 {
     if (empty($name))
@@ -33,6 +22,3 @@ function formatName($name)
 formatName('JOSÉ NORBERTO E SILVA');        // José Norberto e Silva
 formatName('ÂNTONIO sOBRInHO Dos Santos');  // Ântonio Sobrinho dos Santos
 formatName('Jéssica Cunha BraGA Da Costa'); // Jéssica Cunha Braga da Costa
-```
-
-[estevao.io](https://estevao.io)
